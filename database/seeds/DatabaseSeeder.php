34c70dac2424c110aa1066ddcff1c65047a410d8
hadymaher31 <hadymaher31@yahoo.com>
2019-06-09 22:04:26 +0200

    add admin seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Admin;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,5 +13,11 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        $admin = new Admin;
+        $admin->first_name = "Hady";
+        $admin->last_name = "Maher";
+        $admin->email = "hadymaher311@gmail.com";
+        $admin->password = Hash::make('123456789');
+        $admin->save();
     }
 }
