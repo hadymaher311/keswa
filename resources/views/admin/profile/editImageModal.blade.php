b2ce051354a9d4d4d370246823e96a52700640e8
hadymaher31 <hadymaher31@yahoo.com>
2019-06-18 17:37:18 +0200

    change crop lib

--- resources/views/admin/profile/editImageModal.blade.php
+++ resources/views/admin/profile/editImageModal.blade.php
@@ -1,11 +1,6 @@
-<link rel="stylesheet" href="{{ asset('/admin_styles/css/croppie.css') }}">
-<style>
-    .croppie-container .cr-image {
-        position: relative;
-        max-width: 100%;
-    }
-</style>
-<script src="{{ asset('/admin_styles/js/croppie.min.js') }}"></script>
+<link rel="stylesheet" href="{{ asset('/admin_styles/css/cropper.css') }}">
+
+<script src="{{ asset('/admin_styles/js/cropper.js') }}"></script>
 
 <script>
     $(function() {
@@ -17,37 +12,38 @@
         modal_1_body += '<input type="hidden" name="image" class="image-data">';
         modal_1_body += '<input type="file" accept="image/*" class="form-control file-input @error("image") is-invalid @enderror" />';
         modal_1_body += '<br>';
-        modal_1_body += '<button type="submit" class="submit-form btn btn-warning btn-block">{{ __("Submit") }}</button>';
+        modal_1_body += '<button class="submit-form btn btn-warning btn-block">{{ __("Submit") }}</button>';
         modal_1_body += '</form>';
         $("#modal-1").fireModal({
           title: '{{ __("Change image") }}',
           body: modal_1_body
         });
-        
+        var $Image;
+        var crop;
         $('.modal').on('change', '.file-input', function(e) {
             let image = e.target.files[0];
             let reader = new FileReader();
             reader.readAsDataURL(image);
             reader.onload = e => {
-                $('.image-preview-demo .cr-boundary').remove()
-                $('.image-preview-demo .cr-slider-wrap').remove()
                 $('.image-preview-demo').html('<img src="" alt="" class="img-fluid preview">')
                 $('.image-preview-demo img.preview').attr('src', e.target.result)
-                crop = $('.image-preview-demo img.preview').croppie({
-                    enableExif: true,
-                    enableResize: true,
-                    viewport: { width: 200, height: 200, type: 'square' }
-                });
+                $Image = $('.image-preview-demo img.preview')
+                crop = $Image.cropper({
+                    aspectRatio: 1 / 1,
+                    dragMode: 'move',
+                    autoCropArea: 1.0,
+                    cropBoxMovable: false
+                    });
             }
         })
         
-        $('.modal').on('click', '.submit-form', function(e) {
-            e.preventDefault();
-            var that = $(this);
-            crop.croppie('result', 'base64').then((image_data) => {
-                $("input.image-data").val(image_data);
-                that.parents('form').submit();
-            })
+        $('.modal').on('click', '.submit-form', function(event) {
+            event.preventDefault();
+            
+            $(".image-data").val($Image.cropper('getCroppedCanvas').toDataURL());
+            $(this).parents('form').submit();
+            
+            
         })
         
     })
