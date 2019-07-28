de18e4265969045e166d089b70a3ea174431c2b4
hadymaher31 <hadymaher31@yahoo.com>
2019-07-29 01:27:35 +0200

    pos workers related models

--- app/Models/POSWorkerAddress.php
+++ app/Models/POSWorkerAddress.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class POSWorkerAddress extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'country',
+        'city',
+        'street',
+        'building',
+        'floor',
+        'apartment',
+        'nearest_landmark',
+        'location_type',
+        'admin_id'
+    ];
 }
--- app/Models/POSWorkerPersonalInfo.php
+++ app/Models/POSWorkerPersonalInfo.php
@@ -6,5 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class POSWorkerPersonalInfo extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'gender', 'phone', 'birth_date', 'admin_id'
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'date_of_birth' => 'datetime',
+    ];
 }
